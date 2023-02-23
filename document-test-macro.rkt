671e5c36edf8ca376de491365504297611fa0626
tawashen <tawashen@gmail.com>
2023-02-23 11:44:14 +0900

    varivariant追加

--- document-test-macro.rkt
+++ document-test-macro.rkt
@@ -1,25 +1,27 @@
 #lang racket
 
-#|
+(require describe)
+
 (define-syntax varivariant
   (syntax-rules ()
     ((_ id) (if (number? id) 'number
                 (if (symbol? id) 'symbol
                     (if (list? id) 'list
                          (if (string? id) 'string
-                              (if struct? id)
-                              (with-handlers ([exn:fail
-                              ;ここでエラーを起こしてエラーメッセージを捉えたい              
-                'void))))))))))
- |#               
+                             (if (struct? id)
+                                 (string->symbol (string-trim (symbol->string (vector-ref (struct->vector id) 0)) "struct:"))
+                                 ;ここに考えられる限りの術後を追加する
+                                  'void))))))))
 
-(struct tawa (name adress))
+
+
+
+
+(struct tawa (name adress) #:transparent)
 (define ore (tawa "tawa" "hosida"))
-ore
 
-
-
-
+(varivariant ore)
+(variant ore)
 
 
              
