c34f13f342459a9fc0f497d222558e8fac1f44af
tawashen <tawashen@gmail.com>
2023-05-10 23:14:05 +0900

    map更新の途中

--- 4king/4king-util.rkt
+++ 4king/4king-util.rkt
@@ -9,3 +9,23 @@
   (() (input ""))
   ((prompt) (display prompt)(newline)
        (read-line))))
+
+
+;;;;;;;;;;;;;;;;;;移動用チェック述語
+(define (hidari-ue? current)
+  (if (= 0 current) #f #t))
+(define (migi-ue? current)
+  (if (= 6 current) #f #t))
+(define (hidari-sita? current)
+  (if (= 42 current) #f #t))
+(define (migi-sita? current)
+  (if (= 48 current) #f #t))
+(define (hidari? current)
+  (if (= 0 (modulo current 7)) #f #t))
+(define (migi? current)
+  (if (= 0 (modulo (+ 1 current) 7)) #f #t))
+(define (ue? current)
+  (if (> 7 current) #f #t))
+(define (sita? current)
+  (if (< 42 current) #f #t))
+
