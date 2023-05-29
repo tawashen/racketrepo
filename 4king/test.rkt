35c835233c6e7ce8da1c762c438e45223b0afc3d
tawashen <tawashen@gmail.com>
2023-05-29 23:30:04 +0900

    対戦テスト成功

--- 4king/test.rkt
+++ 4king/test.rkt
@@ -15,25 +15,34 @@
 ;(list-index 3 '(1 2 3 4) 0)
 
 
-(define (battle-zero car-command-list enemy-attack-list counter damage-list);counterは一つ上のLoopで1から増やす
+;LIST内の特定の要素の場所を返す
+(define (list-index2 num lst count)
+  (cond ((null? lst) #f)
+         ((= num (car lst)) count)
+         (else (list-index num (cdr lst) (+ 1 count)))))
+
+
+;(battle-zero 2 '(1 1 2) '(1 1 2) 1 '() 1)
+
+(define (battle-zero car-command-list enemy-attack-list p-count e-count damage-list);counterは一つ上のLoopで1から増やす
   (cond ((null? enemy-attack-list) (reverse damage-list))
-        ((= (+ counter car-command-list)  (car enemy-attack-list))
-         (battle-zero car-command-list (cdr enemy-attack-list) counter
+        ((and (= e-count car-command-list) (= p-count (car enemy-attack-list)));taiman
+         (battle-zero car-command-list (cdr enemy-attack-list) p-count (+ 1 e-count)
                       (cons "taiman" damage-list)))
-        ((and (not (equal? car-command-list (car enemy-attack-list))) (= counter (car enemy-attack-list)))
-         (battle-zero car-command-list (cdr enemy-attack-list) counter
+              ((= p-count (car enemy-attack-list))
+         (battle-zero car-command-list (cdr enemy-attack-list) p-count (+ 1 e-count)
          (cons "bousen" damage-list)))
-        ((and (not (equal? car-command-list (car enemy-attack-list))) (not (equal? 
-        ((and (not (equal? car-command-list (car enemy-attack-list))) (not (equal? (+ 1 counter) (car enemy-attack-list))))
-         (battle-zero car-command-list (cdr enemy-attack-list) counter
+              ((and (not (= p-count (car enemy-attack-list))) (= e-count car-command-list))  
+              (battle-zero car-command-list (cdr enemy-attack-list) p-count (+ 1 e-count)
          (cons "ippouteki" damage-list)))
-        (else 
+        (else  (battle-zero car-command-list (cdr enemy-attack-list)  p-count (+ 1 e-count)
+         (cons "nanimonai" damage-list)))))
 
-;(battle-zero 2 '(2 3 1) 1 '())
+(battle-zero 1 '(3 2 1) 1 1 '())
 
-(define (battle-map command-list enemy-attack-list counter damage-lists)
+(define (battle-map command-list enemy-attack-list p-count damage-lists)
   (if (null? command-list) (reverse damage-lists)
-      (battle-map (cdr command-list) enemy-attack-list (+ 1 counter)
-                  (cons (battle-zero (car command-list) enemy-attack-list counter '()) damage-lists))))
+      (battle-map (cdr command-list) enemy-attack-list (+ 1 p-count)
+                  (cons (battle-zero (car command-list) enemy-attack-list p-count 1 '()) damage-lists))))
 
-(battle-map '(1 1 2 3) '(2 1 3 3) 0 '())
+(battle-map '(1 2 3 4) '(2 2 3 4) 1 '())
